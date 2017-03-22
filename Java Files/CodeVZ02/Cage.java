0fe0b8c10b4011012ace9dcf3f059ffee64e84b9
Mikhael Artur <Mikhael Artur>
2017-03-22 14:33:07 +0700

    Driver

--- Java Files/CodeVZ02/Cage.java
+++ Java Files/CodeVZ02/Cage.java
@@ -1,13 +1,11 @@
-class Cage
-{
+class Cage{
 	private int cage_id;
     private int n_animal;
     private int n_cell;
     private int cage_type;
-    private Animal a[];
+	private Animal a[];
     private Cell c[];
-	Cage()
-	{
+	Cage(){
 		n_animal = 0;
 		n_cell = 0;
 		a = new Animal[999];
@@ -15,100 +13,90 @@ class Cage
 		cage_id = -1;
 		cage_type = 0;
 	}
-	public boolean IsFull()
-	{
+	public boolean IsFull(){
 		return (n_animal >= ((n_cell / 10) * 3));
 	}
-	public void AddAnimal(Animal A)
-	{
+	public void AddAnimal(Animal A){
 		A.SetLocationX(c[n_animal].GetLocationX());
 		A.SetLocationY(c[n_animal].GetLocationY());
 		a[n_animal] = A;
 		n_animal +=1;
 	}
-	public void SetCageID(int n)
-	{
+	public void SetCageID(int n){
 		cage_id=n;
 	}
-	public int GetCageID()
-	{
+	public int GetCageID(){
 		return cage_id;
 	}
-	public int GetCageType()
-	{
+	public int GetCageType(){
 		return cage_type;
 	}
-	public Animal IsSpaceOccupied(int x, int y)
-	{
+	public Animal IsSpaceOccupied(int x, int y){
 		Animal ret;
 		int i = 0;
-		while (ret == null && i < n_animal)
-			if (a[i].GetLocationX() == x && a[i].GetLocationY()== y)
+		while (ret == null && i < n_animal){
+			if (a[i].GetLocationX() == x && a[i].GetLocationY()== y){
 				ret = a[i];
-			else
+			}
+			else{
 				i += 1;
+			}
+		}
 		return ret;	
 	}
 	public int CountFoodHerbivore()
 	{
 		int i;
 		int sum = 0;
-		for(i=0;i<n_animal;i++)
-		{
-			if (a[i].GetFoodType==1)
+		for(i=0;i<n_animal;i++){
+			if (a[i].GetFoodType==1){
 				sum += a[i].GetFoodQuantity();
+			}
 		}
 		return sum;
 	}
-	public int CountFoodCarnivore()
-	{
+	public int CountFoodCarnivore(){
 		int i;
 		int sum = 0;
-		for(i=0;i<n_animal;i++)
-		{
-			if (a[i].GetFoodType==2)
+		for(i=0;i<n_animal;i++){
+			if (a[i].GetFoodType==2){
 				sum += a[i].GetFoodQuantity();
+			}
 		}
 		return sum;
 	}
-	public int CountFoodOmnivore()
-	{
+	public int CountFoodOmnivore(){
 		int i;
 		int sum = 0;
-		for(i=0;i<n_animal;i++)
-		{
-			if (a[i].GetFoodType==3)
+		for(i=0;i<n_animal;i++){
+			if (a[i].GetFoodType==3){
 				sum += a[i].GetFoodQuantity();
+			}
 		}
 		return sum;
 	}
-	public void AddHabitat(Cell H)
-	{
-		if (n_cell==0)
-		{
+	public void AddHabitat(Cell H){
+		if (n_cell==0){
 			cage_type = H.GetCellID();
 			c[n_cell] = H;
 			n_cell += 1;
 			H.SetCageID(cage_id);
 		}
-		else
-		{
-			if (cage_type == H.GetCellID())
-			{
+		else{
+			if (cage_type == H.GetCellID()){
 				c[n_cell] = H;
 				n_cell += 1;
 				H.SetCageID(cage_id);
 			}
 		}
 	}
-	public boolean IsExist(int ID)
-	{
+	public boolean IsExist(int ID){
 		boolean found = false;
 		int i = 0;
-		while (i < n_animal)
-		{
-			if (a[i].GetType() == ID)
+		while (i < n_animal){
+			if (a[i].GetType() == ID){
 				found = true;
+			}
 			i++;
 		}
 		return found;
