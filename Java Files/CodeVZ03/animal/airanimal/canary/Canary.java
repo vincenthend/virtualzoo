101bf0a1cc422b2a05e175b7f466198b354c762b
vincenthend <vincenthendrha@gmail.com>
2017-03-26 18:04:10 +0700

    VZ03 Cell Fixed

--- Java Files/CodeVZ03/animal/airanimal/canary/Canary.java
+++ Java Files/CodeVZ03/animal/airanimal/canary/Canary.java
@@ -1,25 +1,24 @@
-	/*
-	* @author Vincent Hendryanto H/13515089
-	* Class Canary, mengatur konstruksi Canary
-	*/
-	
-package animal. canary;
-import animal;
-import animal.airanimal;
+/**
+ * @author Vincent Hendryanto H/13515089
+ * Class Canary, mengatur konstruksi Canary
+ */
 
-class Canary extends AirAnimal{
-	/*
-	* Mengatur konstruksi animal dengan spesifikasi yang sudah ditentukan
-	*/
-	Canary() {
-		weight = 1;
-		food_weight = 1;
-		food_type = 1;
-		type = 4;
-		c_enemy = 0;
-		sound = "Kweee";
-		code = "$";
-	}
+package animal.airanimal.canary;
+import animal.airanimal.AirAnimal;
+
+class Canary extends AirAnimal {
+  /**
+   * Mengatur konstruksi animal dengan spesifikasi yang sudah ditentukan
+   */
+  Canary() {
+    weight = 1;
+    food_weight = 1;
+    food_type = 1;
+    type = 4;
+    c_enemy = 0;
+    sound = "Kweee";
+    code = "$";
+  }
 }
 
 
