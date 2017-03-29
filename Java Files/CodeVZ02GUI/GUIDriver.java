61a4323d731558d97b53f35c8d8faee4cb72aa67
vincenthend <vincenthendrha@gmail.com>
2017-03-29 10:45:05 +0700

    GUI 40%

--- Java Files/CodeVZ02GUI/GUIDriver.java
+++ Java Files/CodeVZ02GUI/GUIDriver.java
@@ -1,19 +1,22 @@
 import animal.Animal;
 import cell.Cell;
 import java.awt.Button;
+import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-import javax.swing.*;
-
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 import zoo.Zoo;
 
 /**
@@ -28,10 +31,10 @@ public class GUIDriver {
   private JFrame mainWindow;
   private JPanel zooMap;
   private JPanel buttonPane;
+  private JPanel statusPane;
   private JLabel[][] mapLabel;
   private JLabel interactLabel;
   private boolean nextStep;
-
 
   /**
    * Inisialisasi driver dengan membaca dari file inputFile
@@ -137,9 +140,7 @@ public class GUIDriver {
   }
 
   public void printInitial() {
-    mainWindow = new JFrame("VirtualZoo");
-    mainWindow.setSize((zoo.getWidth() * 25 * 3) / 2, zoo.getHeight() * 25);
-    mainWindow.setLayout(new GridLayout(0, 2));
+    initWindow();
 
     //Print Map
     printZoo();
@@ -147,91 +148,39 @@ public class GUIDriver {
 
     //Button Pane
     buttonPane = new JPanel();
-    buttonPane.setLayout(new GridLayout(4,0));
+    buttonPane.setLayout(new GridLayout(4, 0));
 
     Button startTour = new Button("Start Tour");
     buttonPane.add(startTour);
     mainWindow.add(buttonPane);
-    mainWindow.setVisible(true);
 
+    mainWindow.setVisible(true);
     //Action Button
-    startTour.addActionListener(new java.awt.event.ActionListener () {
-      public void actionPerformed (java.awt.event.ActionEvent evt) {
+    startTour.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
         try {
-          startTourClick ();
+          startTourClick();
+          mainWindow.getContentPane().removeAll();
+          mainWindow.revalidate();
+          mainWindow.repaint();
+          mainWindow.setVisible(true);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
       }
     });
-    interactLabel = new JLabel("",JLabel.CENTER);
-    interactLabel.setSize(350,100);
+    //interactLabel = new JLabel("",JLabel.CENTER);
+    //interactLabel.setSize(350,100);
   }
+
+  public void initWindow() {
+    mainWindow = new JFrame("VirtualZoo");
+    mainWindow.setSize((zoo.getWidth() * 25 * 3) / 2, zoo.getHeight() * 25);
+    mainWindow.setLayout(new GridLayout(0, 2));
+  }
+
   private void startTourClick() throws InterruptedException {
     startTour();
-  }
-  /**
-   * Menuliskan menu ke layar, menerima pilihan menu dan mengeksekusi pilihan
-   * I. S.: Sembarang
-   * F. S.: Menu tertulis di layar, pilihan diterima dan dieksekusi
-   */
-  public void printMenu() throws InterruptedException {
-    int choice;
-    int x1;
-    int x2;
-    int y1;
-    int y2;
-    Scanner in = new Scanner(System.in);
-    do {
-      System.out.println("=========================================");
-      System.out.println("|\t\t\t\t\t\t\t\tVIRTUAL ZOO\t\t\t\t\t\t\t\t|");
-      System.out.println("=========================================");
-      System.out.println("Menu :");
-      System.out.println("1. Tampilkan Zoo");
-      System.out.println("2. Mulai Tour");
-      System.out.println("9. Keluar");
-      System.out.println("Pilihan : ");
-      choice = in.nextInt();
-      in.nextLine();
-      if (choice == 1) {
-        do {
-          System.out.println("=========================================");
-          System.out.println("|\t\t\t\t\t\t\t\tVIRTUAL ZOO\t\t\t\t\t\t\t\t|");
-          System.out.println("=========================================");
-          System.out.println("1. Tampilkan seluruhnya");
-          System.out.println("2. Tampilkan sebagian");
-          System.out.println("9. Keluar");
-          System.out.println("Pilihan : ");
-          choice = in.nextInt();
-          in.nextLine();
-          if (choice == 1) {
-            printZoo();
-            printStatus();
-          } else if (choice == 2) {
-            System.out.println("Masukkan ukuran");
-            System.out.println("X(0 - " + (zoo.getWidth() - 1) + ")");
-            System.out.println("Y(0 - " + (zoo.getHeight() - 1) + ")");
-            System.out.println("X1 :");
-            x1 = in.nextInt();
-            in.nextLine();
-            System.out.println("Y1 :");
-            y1 = in.nextInt();
-            in.nextLine();
-            System.out.println("X2 :");
-            x2 = in.nextInt();
-            in.nextLine();
-            System.out.println("Y2 :");
-            y2 = in.nextInt();
-            in.nextLine();
-            printZoo(x1, x2, y1, y2);
-            System.out.println("");
-            choice = 0;
-          }
-        } while (choice != 9);
-      } else if (choice == 2) {
-        startTour();
-      }
-    } while (choice != 9);
   }
 
   /**
@@ -257,7 +206,7 @@ public class GUIDriver {
     //Print Map
     zooMap = new JPanel();
     zooMap.setLayout(new GridBagLayout());
-    zooMap.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+    zooMap.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
 
     GridBagConstraints c = new GridBagConstraints();
     c.ipadx = 5;
@@ -306,7 +255,7 @@ public class GUIDriver {
     //Print Map
     zooMap = new JPanel();
     zooMap.setLayout(new GridBagLayout());
-    zooMap.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+    zooMap.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
 
     GridBagConstraints c = new GridBagConstraints();
     c.ipadx = 5;
@@ -323,10 +272,9 @@ public class GUIDriver {
       for (j = 0; j < zoo.getWidth(); j++) {
         c.gridx = j;
         c.gridy = i;
-        if(j==x && i==y){
+        if (j == x && i == y) {
           symbol = "X";
-        }
-        else {
+        } else {
           cell = zoo.getCell(i, j);
           if (cell != null) {
             if (cell.getCageId() > -1) {
@@ -341,57 +289,6 @@ public class GUIDriver {
             }
           }
         }
-        mapLabel[i][j] = new JLabel(symbol);
-        zooMap.add(mapLabel[i][j], c);
-      }
-    }
-  }
-
-  /**
-   * Menuliskan peta kebun binatang dari range x1-x2,y1-y2
-   * I. S.: x1 lebih kecil dari x2, y1 lebih kecil dari y2
-   * F. S.: Peta kebun binatang tertulis di layar
-   *
-   * @param x1 Titik mulai x
-   * @param x2 Titik akhir x
-   * @param y1 Titik mulai y
-   * @param y2 Titik akhir y
-   */
-  public void printZoo(int x1, int x2, int y1, int y2) {
-    //Print Map
-    zooMap = new JPanel();
-    zooMap.setLayout(new GridBagLayout());
-    zooMap.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-
-    GridBagConstraints c = new GridBagConstraints();
-    c.ipadx = 5;
-    c.ipady = 5;
-
-    //Print Zoo Map
-    int i;
-    int j;
-    String symbol = "";
-    Cell cell;
-    Animal animal;
-    mapLabel = new JLabel[zoo.getHeight()][zoo.getWidth()];
-    for (i = y1; i < y2; i++) {
-      for (j = x1; j < x2; j++) {
-        c.gridx = j;
-        c.gridy = i;
-          cell = zoo.getCell(i, j);
-          if (cell != null) {
-            if (cell.getCageId() > -1) {
-              animal = zoo.getCage(cell.getCageId()).isSpaceOccupied(i, j);
-              if (animal != null) {
-                symbol = animal.render();
-              } else {
-                symbol = cell.render();
-              }
-            } else {
-              symbol = cell.render();
-            }
-          }
-
         mapLabel[i][j] = new JLabel(symbol);
         zooMap.add(mapLabel[i][j], c);
       }
@@ -407,20 +304,18 @@ public class GUIDriver {
     int[] availMove = new int[4];
     int[] tx = new int[10];
     int[] ty = new int[10];
-    Cell c = null;
-    Animal a;
+    final Cell[] c = {null};
+    final Animal[] a = new Animal[1];
     int tc = 0;
-    printZoo();
-    printStatus();
-    int i;
-    int j;
-    for (i = 0; i < zoo.getWidth(); i++) {
-      for (j = 0; j < zoo.getHeight(); j++) {
-        c = zoo.getCell(i, j);
-        if (c != null) {
-          if (c.getCellId() == 210) {
-            tx[tc] = i;
-            ty[tc] = j;
+    final int[] i = new int[1];
+    final int[] j = new int[1];
+    for (i[0] = 0; i[0] < zoo.getWidth(); i[0]++) {
+      for (j[0] = 0; j[0] < zoo.getHeight(); j[0]++) {
+        c[0] = zoo.getCell(i[0], j[0]);
+        if (c[0] != null) {
+          if (c[0].getCellId() == 210) {
+            tx[tc] = i[0];
+            ty[tc] = j[0];
             tc++;
           }
         }
@@ -428,121 +323,140 @@ public class GUIDriver {
     }
     Random rand = new Random(System.currentTimeMillis());
     int temp = Math.abs((rand.nextInt() % (tc)));
-    int move = 0;
-    int cntMove;
-    i = tx[temp];
-    j = ty[temp];
-    boolean walk = true;
-    while (c.getCellId() != 211 && walk) {
-      //Print Zoo
-      mainWindow.remove(zooMap);
-      printZoo(j, i);
-      mainWindow.add(zooMap);
-      mainWindow.repaint();
-      printStatus();
-      System.out.println("(" + i + "," + j + ")" + c.getCellId());
-      //interact
-      if (((j - 1) >= 0) && (move != 3)) {
-        c = zoo.getCell(i, j - 1);
-        if (c != null) {
-          if ((c.getCellId() >= 11) && (c.getCellId() <= 13)) {
-            if (c.getCageId() > -1) {
-              a = zoo.getCage(c.getCageId()).isSpaceOccupied(i, j - 1);
-              if (a != null) {
-                a.interact();
-              }
-            }
-          }
-        }
-      }
-      if (((i + 1) < zoo.getHeight()) && (move != 4)) {
-        c = zoo.getCell(i + 1, j);
-        if (c != null) {
-          if ((c.getCellId() >= 11) && (c.getCellId() <= 13)) {
-            if (c.getCageId() > -1) {
-              a = zoo.getCage(c.getCageId()).isSpaceOccupied(i + 1, j);
-              if (a != null) {
-                a.interact();
-              }
-            }
-          }
-        }
-      }
-      if (((j + 1) < zoo.getWidth()) && (move != 1)) {
-        c = zoo.getCell(i, j + 1);
-        if (c != null) {
-          if ((c.getCellId() >= 11) && (c.getCellId() <= 13)) {
-            if (c.getCageId() > -1) {
-              a = zoo.getCage(c.getCageId()).isSpaceOccupied(i, j + 1);
-              if (a != null) {
-                a.interact();
-              }
-            }
-          }
-        }
-      }
-      if (((i - 1) >= 0) && (move != 2)) {
-        c = zoo.getCell(i - 1, j);
-        if (c != null) {
-          if ((c.getCellId() >= 11) && (c.getCellId() <= 13)) {
-            if (c.getCageId() > -1) {
-              a = zoo.getCage(c.getCageId()).isSpaceOccupied(i - 1, j);
-              if (a != null) {
-                a.interact();
-              }
-            }
-          }
-        }
-      }
-      cntMove = 0;
-      if (((j - 1) >= 0) && (move != 3)) {
-        c = zoo.getCell(i, j - 1);
-        if (c.getCellId() == 21 || c.getCellId() == 211) {
-          availMove[cntMove] = 1;
-          cntMove++;
-        }
-      }
-      if (((i + 1) < zoo.getHeight()) && (move != 4)) {
-        c = zoo.getCell(i + 1, j);
-        if (c.getCellId() == 21 || c.getCellId() == 211) {
-          availMove[cntMove] = 2;
-          cntMove++;
-        }
-      }
-      if (((j + 1) < zoo.getWidth()) && (move != 1)) {
-        c = zoo.getCell(i, j + 1);
-        if (c.getCellId() == 21 || c.getCellId() == 211) {
-          availMove[cntMove] = 3;
-          cntMove++;
-        }
-      }
-      if (((i - 1) >= 0) && (move != 2)) {
-        c = zoo.getCell(i - 1, j);
-        if (c.getCellId() == 21 || c.getCellId() == 211) {
-          availMove[cntMove] = 4;
-          cntMove++;
-        }
-      }
-      if (cntMove > 0) {
-        move = availMove[Math.abs((rand.nextInt() % cntMove))];
-        if (move == 1) {
-          j--;
+    final int[] move = {0};
+    final int[] cntMove = new int[1];
+    i[0] = tx[temp];
+    j[0] = ty[temp];
+    final boolean[] walk = {true};
+    final Container[] container = {null};
+
+    int delay = 20;
+    Timer timer = new Timer(delay, null);
+    timer.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        if (c[0].getCellId() == 211 || !walk[0]) {
+          timer.stop();
         } else {
-          if (move == 2) {
-            i++;
-          } else {
-            if (move == 3) {
-              j++;
-            } else {
-              i--;
+          //Print Map
+
+          container[0] = mainWindow.getContentPane();
+          container[0].removeAll();
+
+          //Print Zoo
+          printZoo(j[0], i[0]);
+          container[0].add(zooMap);
+
+          mainWindow.setVisible(true);
+          mainWindow.revalidate();
+          mainWindow.repaint();
+          printStatus();
+
+          System.out.println("(" + i[0] + "," + j[0] + ")" + c[0].getCellId());
+          //interact
+          if (((j[0] - 1) >= 0) && (move[0] != 3)) {
+            c[0] = zoo.getCell(i[0], j[0] - 1);
+            if (c[0] != null) {
+              if ((c[0].getCellId() >= 11) && (c[0].getCellId() <= 13)) {
+                if (c[0].getCageId() > -1) {
+                  a[0] = zoo.getCage(c[0].getCageId()).isSpaceOccupied(i[0], j[0] - 1);
+                  if (a[0] != null) {
+                    a[0].interact();
+                  }
+                }
+              }
             }
           }
+          if (((i[0] + 1) < zoo.getHeight()) && (move[0] != 4)) {
+            c[0] = zoo.getCell(i[0] + 1, j[0]);
+            if (c[0] != null) {
+              if ((c[0].getCellId() >= 11) && (c[0].getCellId() <= 13)) {
+                if (c[0].getCageId() > -1) {
+                  a[0] = zoo.getCage(c[0].getCageId()).isSpaceOccupied(i[0] + 1, j[0]);
+                  if (a[0] != null) {
+                    a[0].interact();
+                  }
+                }
+              }
+            }
+          }
+          if (((j[0] + 1) < zoo.getWidth()) && (move[0] != 1)) {
+            c[0] = zoo.getCell(i[0], j[0] + 1);
+            if (c[0] != null) {
+              if ((c[0].getCellId() >= 11) && (c[0].getCellId() <= 13)) {
+                if (c[0].getCageId() > -1) {
+                  a[0] = zoo.getCage(c[0].getCageId()).isSpaceOccupied(i[0], j[0] + 1);
+                  if (a[0] != null) {
+                    a[0].interact();
+                  }
+                }
+              }
+            }
+          }
+          if (((i[0] - 1) >= 0) && (move[0] != 2)) {
+            c[0] = zoo.getCell(i[0] - 1, j[0]);
+            if (c[0] != null) {
+              if ((c[0].getCellId() >= 11) && (c[0].getCellId() <= 13)) {
+                if (c[0].getCageId() > -1) {
+                  a[0] = zoo.getCage(c[0].getCageId()).isSpaceOccupied(i[0] - 1, j[0]);
+                  if (a[0] != null) {
+                    a[0].interact();
+                  }
+                }
+              }
+            }
+          }
+          cntMove[0] = 0;
+          if (((j[0] - 1) >= 0) && (move[0] != 3)) {
+            c[0] = zoo.getCell(i[0], j[0] - 1);
+            if (c[0].getCellId() == 21 || c[0].getCellId() == 211) {
+              availMove[cntMove[0]] = 1;
+              cntMove[0]++;
+            }
+          }
+          if (((i[0] + 1) < zoo.getHeight()) && (move[0] != 4)) {
+            c[0] = zoo.getCell(i[0] + 1, j[0]);
+            if (c[0].getCellId() == 21 || c[0].getCellId() == 211) {
+              availMove[cntMove[0]] = 2;
+              cntMove[0]++;
+            }
+          }
+          if (((j[0] + 1) < zoo.getWidth()) && (move[0] != 1)) {
+            c[0] = zoo.getCell(i[0], j[0] + 1);
+            if (c[0].getCellId() == 21 || c[0].getCellId() == 211) {
+              availMove[cntMove[0]] = 3;
+              cntMove[0]++;
+            }
+          }
+          if (((i[0] - 1) >= 0) && (move[0] != 2)) {
+            c[0] = zoo.getCell(i[0] - 1, j[0]);
+            if (c[0].getCellId() == 21 || c[0].getCellId() == 211) {
+              availMove[cntMove[0]] = 4;
+              cntMove[0]++;
+            }
+          }
+          if (cntMove[0] > 0) {
+            move[0] = availMove[Math.abs((rand.nextInt() % cntMove[0]))];
+            if (move[0] == 1) {
+              j[0]--;
+            } else {
+              if (move[0] == 2) {
+                i[0]++;
+              } else {
+                if (move[0] == 3) {
+                  j[0]++;
+                } else {
+                  i[0]--;
+                }
+              }
+            }
+          } else {
+            walk[0] = false;
+          }
+          c[0] = zoo.getCell(i[0], j[0]);
         }
-      } else {
-        walk = false;
       }
-      c = zoo.getCell(i, j);
-      TimeUnit.SECONDS.sleep(1);
-    }
+    });
+    timer.start();
+    container[0].add(buttonPane);
   }
 }
